025e21806813186e6615638abd3d6205acf3be4a
Rajnish Kumar <rajnishmsrit@gmail.com>
2017-06-17 12:59:09 +0530

    Basic setup with autoloading, composer, namespaces, and phpunit

---
+++ app/code/Phpunit/Data.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Code\Phpunit;
+
+Class Data
+{
+  /**
+   * [getTotal description]
+   * @return [type] [description]
+   */
+  public function getTotal()
+  {
+    return true;
+  }
+}
