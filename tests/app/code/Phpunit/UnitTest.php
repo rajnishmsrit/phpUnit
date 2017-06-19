0f062fa02532c19e45004a791d3d81dc06b22aa2
Rajnish Kumar <rajnishmsrit@gmail.com>
2017-06-19 16:31:36 +0530

    Commit at fixtures

---
+++ tests/app/code/Phpunit/UnitTest.php
@@ -0,0 +1,24 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class Tester extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 2]
+        ];
+    }
+}
+?>
